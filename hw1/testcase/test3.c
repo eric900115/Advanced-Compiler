5f90f77467058a7db44dc9088afe37e2c2da52a1
eric900115 <eric20607@gmail.com>
2023-11-06 16:51:27 +0800

    [TestCase] : Generate Testcase for HW1

---
+++ hw1/testcase/test3.c
@@ -0,0 +1,10 @@
+//test3.c
+int main(){
+    int i;
+    int A[70], B[70], C[70];
+    for (i =1; i < 70; i+=5) {
+        A[i] = C[i];
+        B[i] = A[i - 4];
+    }
+    return 0;
+}
---
+++ hw1/testcase/test4.c
@@ -0,0 +1,13 @@
+//test4.c
+int main(){
+    int i;
+    int A[80], B[80], C[80], D[80], E[30], F[80];
+    for (i =5; i < 30; i+=5) {
+        A[i] = C[i];
+        B[i] = A[i - 4];
+        D[i] = B[5 * i];
+        E[i] = A[2 * i + 10];
+        E[i - 5] = B[i];
+    }
+    return 0;
+}
